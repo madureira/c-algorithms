59d7dfb349c22bec90b3bb5d6924422e18b2dd6e
madureira <rafael.madureira.santos@gmail.com>
2020-07-24 13:19:05 -0300

    Add more complete comment description to ArrayList

--- data_structures/ArrayList/test.c
+++ data_structures/ArrayList/test.c
@@ -1,5 +1,6 @@
 /*
-ArrayList: Dynamic sized arrays
+An ArrayList is a random access, variable-size list data structure that
+allows elements to be added or removed and resizes itself automatically.
 */
 #include <stdio.h>
 #include <string.h>
